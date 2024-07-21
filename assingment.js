97ffe843f9c8040fbc13ed6fcc000344261c71ad
soojal-bhardwaj-27 <soojal.bhardwaj.27@gmail.com>
2024-07-21 08:51:18 +0530

    assingment

---
+++ assingment.js
@@ -0,0 +1,11 @@
+    //Write a program to check if a number is positive, negative, or zero
+     
+    let num = parseInt(prompt("Enter the number."));
+if (num > 0) {
+    alert("num is positive");
+} else if (num == 0) {
+    alert("zero");
+} else {
+    alert("negative");
+}
+
